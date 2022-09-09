8c188279ed9b534dbeea5794bcf3db72a2f14f19
Natalya Lobanova <knopka40a@mail.ru>
2022-09-09 13:37:58 +0500

    Add Ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int Ackerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return Ackerman(m - 1, 1);
+    else if (m > 0 && n > 0) return Ackerman(m - 1, Ackerman(m, n - 1));
+    return Ackerman(m, n);
+}
+Console.WriteLine(Ackerman(2, 3));
