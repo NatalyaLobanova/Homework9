d2e36149dd62e3498eb600ea50e526cdbad91ec2
Natalya Lobanova <knopka40a@mail.ru>
2022-09-09 13:11:49 +0500

    Add Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+string NumbersRec(int m, int n)
+{
+    if (m <= n) return $"{m} " + NumbersRec(m + 1, n);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec(1, 10));
+
