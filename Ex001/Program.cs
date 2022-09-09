c00ea110259e95a22c10390dd73cc4a510734606
Natalya Lobanova <knopka40a@mail.ru>
2022-09-09 12:59:17 +0500

    Add Ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+string NumbersRec(int n)
+{
+    if (n < 1) return string.Empty;
+    else return $"{n} " + NumbersRec(n - 1);   
+}
+
+Console.WriteLine(NumbersRec(15));
