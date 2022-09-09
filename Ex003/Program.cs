fd125dbd0e8dcce67ad2971485b2a7c875a48bc4
Natalya Lobanova <knopka40a@mail.ru>
2022-09-09 13:27:51 +0500

    Add Ex003

---
+++ Ex003/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumRec(int m, int n)
+{
+    if (m == n) return n;
+    else if (m > n) return m + SumRec(m - 1, n);
+    else return  m + SumRec(m + 1, n);
+}
+Console.WriteLine(SumRec(1, 5));
+
+
